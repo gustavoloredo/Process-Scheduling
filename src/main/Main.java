632f9f7f53dd4532a4409c674bb082fdd8370d74
Otavio Henrique <Otavio.Pires.Costa@ibm.com>
2018-05-17 10:06:18 -0300

    Projeto final para entrega, faltando apenas otimização

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,112 +21,150 @@ public class Main {
 	
 	public static Processo CPU = null;
 		
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException, InterruptedException {
 		Scanner scan = new Scanner(System.in);
 		boolean quantum = false;
 		int maiorPrioridade = 0;
-		String algoritmo = scan.nextLine();
+		int algoritmo = Integer.MIN_VALUE;
+		System.out.println("Bem vindo ao simulador de processos! \n"
+				+ "Digite qual metodo de escalonamento deseja selecionar: \n"
+				+ "1 - Fifo\n"
+				+ "2 - SJF\n"
+				+ "3 - Circular\n"
+				+ "4 - Prioridade\n"
+				+ "5 - Circular com Prioridade\n"
+				+ "Qualquer outro numero - Rodara todos para realizar a comparação");
+		do {
+			while(!scan.hasNextInt()) {
+				scan.next();
+				System.out.println("Digite um numero valido\n");
+			}
+			algoritmo = scan.nextInt();
+		}while(algoritmo == Integer.MIN_VALUE);
 		AlgoritmoEscalonamento ae;
-		
+		int counter = 5;
 		switch(algoritmo) {
 			
-		case "SJF":
+		case 1:
 			ae = new SJF();
 			break;
-		case "FIFO":
+		case 2:
 			ae = new FIFO();
 			break;
-		case "Circular":
+		case 3:
 			ae = new Circular();
 			quantum = true;
 			break;
-		case "Prioridade":
+		case 4:
 			ae = new Prioridades();
 			break;
-		case "PrioCircular":
+		case 5:
 			ae = new CircularPrioridades();
 			quantum = true;
 			break;
 		default:
 			ae = new FIFO();
-			
+			counter = 0;
 		}
-		HashMap<Integer, Processo> processos = criaProcessos(quantum);
-		List<Processo> listaProcessosTerminados = new ArrayList<Processo>();
-		for (int i = 1; i < 100 ; i++) {
-			
-			if (processos.get(i) != null) {
-				Processo processo = processos.get(i);
-				ae.adicionaProcesso(processo);
-				System.out.println("##### " + i + " Criando novo processo: " + processo.getNome());
-			}
-			
-			if (CPU == null && !ae.filaVazia()) {
-				Processo processo = ae.escalonaProcesso();
-				processo.setEstado(Estado.EXECUCAO);
-				System.out.println("##### " + i + "  Escalonando processo: " + processo.getNome());
-				CPU = processo;
-			}
-			
-			if (CPU != null) {
-				Processo processo = CPU;
-				maiorPrioridade = ae.verificaPrioridade();
-				if(algoritmo == "PrioCircular" || algoritmo == "Prioridade") {
-					maiorPrioridade = ae.verificaPrioridade();
-				}
-				processo.setTempoExecucao(processo.getTempoExecucao() + 1);
-				if(maiorPrioridade == processo.getPrioridade() ) {
-					if (processo.getTempoExecucao() == processo.getTempoEntradaSaida()) {
-						CPU = null;
-						System.out.println("##### " + i + "  Processo Entrada/Saída: " + processo.getNome());
-						ae.adicionaProcesso(processo);
-					}				
-				}
-				if(false){
-					if (processo.getTempoExecucao() == processo.getTempoEntradaSaida() ) {
-						CPU = null;
-						System.out.println("##### " + i + "  Processo Entrada/Saída: " + processo.getNome());
-						ae.adicionaProcesso(processo);
-					}		
-					
-				}
-				if (processo.getDuracao() == processo.getTempoExecucao()) {
-					CPU = null;
-					System.out.println("##### " + i + "  Processo Terminado: " + processo.getNome());
-					listaProcessosTerminados.add(processo);
+		do {
+			HashMap<Integer, Processo> processos = criaProcessos(quantum);
+			List<Processo> listaProcessosTerminados = new ArrayList<Processo>();
+			for (int i = 1; i < 100 ; i++) {
+				
+				if (processos.get(i) != null) {
+					Processo processo = processos.get(i);
+					ae.adicionaProcesso(processo);
+					System.out.println("##### " + i + " Criando novo processo: " + processo.getNome());
 				}
 				
-				if(maiorPrioridade > processo.getPrioridade() ) {
-					System.out.println("#### " +i+ " maior prioridade: "+ maiorPrioridade);
-					System.out.println(processo.getPrioridade());
-					CPU = null;
-					System.out.println("##### " + i + "  Processo Entrada/Saída: " + processo.getNome());
-					ae.adicionaProcesso(processo);
+				if (CPU == null && !ae.filaVazia()) {
+					Processo processo = ae.escalonaProcesso();
+					processo.setEstado(Estado.EXECUCAO);
+					System.out.println("##### " + i + "  Escalonando processo: " + processo.getNome());
+					CPU = processo;
 				}
+				
+				if (CPU != null) {
+					Processo processo = CPU;
+					if(algoritmo == 5 || algoritmo == 4) {
+						maiorPrioridade = ae.verificaPrioridade();
+					}
+					processo.setTempoExecucao(processo.getTempoExecucao() + 1);
+					if(maiorPrioridade == processo.getPrioridade() ) {
+						if (processo.getTempoExecucao() == processo.getTempoEntradaSaida()) {
+							CPU = null;
+							System.out.println("##### " + i + "  Processo Entrada/Saída: " + processo.getNome());
+							ae.adicionaProcesso(processo);
+						}				
+					}
+					if(algoritmo !=5 && algoritmo != 4){
+						if (processo.getTempoExecucao() == processo.getTempoEntradaSaida() ) {
+							CPU = null;
+							System.out.println("##### " + i + "  Processo Entrada/Saída: " + processo.getNome());
+							ae.adicionaProcesso(processo);
+						}		
+						
+					}
+					if (processo.getDuracao() == processo.getTempoExecucao()) {
+						CPU = null;
+						System.out.println("##### " + i + "  Processo Terminado: " + processo.getNome());
+						listaProcessosTerminados.add(processo);
+					}
+					
+					if(maiorPrioridade > processo.getPrioridade() ) {
+						System.out.println(processo.getPrioridade());
+						CPU = null;
+						System.out.println("##### " + i + "  Processo Entrada/Saída: " + processo.getNome());
+						ae.adicionaProcesso(processo);
+					}
+				}
+				
+				if (i % 10 == 0) {
+					ae.imprimeEstadoFila();
+				}
+				
+				ae.incrementaTempoEspera();
+				
 			}
 			
-			if (i % 10 == 0) {
-				ae.imprimeEstadoFila();
+			
+			System.out.println("\n\n\n#### Tempos de espera: ");
+			int somaEspera = 0;
+			for (Processo processo : listaProcessosTerminados) {
+				System.out.println(processo.getTempoEspera());
+				somaEspera += processo.getTempoEspera();
 			}
-			
-			ae.incrementaTempoEspera();
-			
-		}
-		
-		
-		System.out.println("\n\n\n#### Tempos de espera: ");
-		int somaEspera = 0;
-		for (Processo processo : listaProcessosTerminados) {
-			System.out.println(processo.getTempoEspera());
-			somaEspera += processo.getTempoEspera();
-		}
-		System.out.println("Tempo Médio: " + (somaEspera / listaProcessosTerminados.size()));
-
-		System.out.println("Tempo Médio: " + (listaProcessosTerminados.size()));
-		
+			String tempo = "Tempo Médio: " + (somaEspera / listaProcessosTerminados.size());
+			System.out.println(tempo);
+		}while(counter < 5);
+		executeInTerminal("clear;"
+				+ " echo "+ tempo);
 		
 		
 	}
+	public static int executeInTerminal(String command) throws IOException, InterruptedException {
+		 final String[] wrappedCommand;
+		 boolean isMac = true, isWindows = false, isLinux = false;
+		 
+		    if (isWindows) {
+		        wrappedCommand = new String[]{ "cmd", "/c", "start", "/wait", "cmd.exe", "/K", command };
+		    }
+		    else if (isLinux) {
+		        wrappedCommand = new String[]{ "xterm", "-e", "bash", "-c", command};
+		    }
+		    else if (isMac) {
+		        wrappedCommand = new String[]{"osascript",
+		                "-e", "tell application \"Terminal\" to activate",
+		                "-e", "tell application \"Terminal\" to do script \"" + command + ";exit\""};
+		    }
+		    else {
+		        throw new RuntimeException("Unsupported OS ☹");
+		    }
+		    Process process = new ProcessBuilder(wrappedCommand)
+		            .redirectErrorStream(true)
+		            .start();
+		    return process.waitFor();
+}
 
 	private static HashMap<Integer, Processo> criaProcessos(boolean quantum) {
 		HashMap<Integer, Processo> processos = new HashMap<Integer, Processo>();
